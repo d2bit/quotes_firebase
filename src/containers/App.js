35a1a38fcc36a74a8c9b7db60c012a736924e141
d2bit <burretfresket@gmail.com>
2018-04-27 08:26:53 +0200

    Add create-react-app

---
+++ src/containers/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">QUOTES</h1>
+        </header>
+        <div className="App-content">
+          <div>
+            <p>This is where magic will happen...</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App
